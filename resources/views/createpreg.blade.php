9a731d63e21197683e647b98cdfb33f16f00374a
acontrerasc2014 <acontrerasc2014@alu.uct.cl>
2018-11-08 01:23:31 -0300

    uso de section vistas createcita, createcom, createpre, createreflex, createresp y createtema

--- resources/views/createpreg.blade.php
+++ resources/views/createpreg.blade.php
@@ -1,24 +1,12 @@
-<!doctype <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Pregunta</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
 
-</head>
-<body>
+@extends('layouts.diseño')
 
-    <h1>Ingrese su pregunta</h1>
-    
-    <form class="form-group" action="/pregunta" method="POST">
-         {!!csrf_field() !!} 
-        <input type="text" name="preg" id="preg"> <span> Ingresa pregunta</span> <br> 
-        <br>
-        <input type="submit" name="submit" value="Enviar!" onclick=alert("Pregunta-Enviada")  >
+@section('crear-pregunta')
 
-    </form>
- 
-</body>
-</html>
+<h1>Ingrese su pregunta</h1>
+<form class="form-group" action="/pregunta" method="POST">
+    {!!csrf_field() !!} 
+    <input type="text" name="preg" id="preg"> <span> Ingresa pregunta</span> <br> 
+    <br>
+    <input type="submit" name="submit" value="Enviar!" onclick=alert("Pregunta-Enviada")  >
+</form>
--- resources/views/createresp.blade.php
+++ resources/views/createresp.blade.php
@@ -1,22 +1,11 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Respuestas</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-</head>
-<body>
-    <h1>Crear respuestas</h1>
-    <form class="form-group" action="/respuesta" method="POST">
-         {!!csrf_field() !!} 
-        <input type="text" name="resp" id="resp"> <span> Ingresa respuesta</span> <br> 
-        <br>
-        <input type="submit" name="submit" value="Enviar!" onclick=alert("Respuesta-Enviada")  >
+@extends('layouts.diseño')
 
-</form>
-    
-</body>
-</html>
+@section('crear-respuestas')
+
+<h1>Crear respuestas</h1>
+<form class="form-group" action="/respuesta" method="POST">
+    {!!csrf_field() !!} 
+    <input type="text" name="resp" id="resp"> <span> Ingresa respuesta</span> <br> 
+    <br>
+<input type="submit" name="submit" value="Enviar!" onclick=alert("Respuesta-Enviada")  >
