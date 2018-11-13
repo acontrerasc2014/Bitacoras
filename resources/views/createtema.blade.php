2d38c89fbddfc8ac5795f82b5e49cc6708878e2f
Diego <dflores2017@alu.uct.cl>
2018-11-13 12:43:21 -0300

    Listo

--- resources/views/createtema.blade.php
+++ resources/views/createtema.blade.php
@@ -10,7 +10,7 @@
 		<span>Nombre:</span> <input type="text" placeholder="EJ: John Titor" name="nombre" id="nombre"><br>
 		<span>Email:</span><br><input type="email" placeholder="EJ: ejemplo@mail.com" name="email" id="email"><br><br>
 		<span>Tema:</span><input type="text" placeholder="EJ: La tarea de hoy" name="titulo" id="titulo"><br> <br>
-		<span>Descripcion:</span><input type="text" placeholder="Cuentanos mas de tu tema" size="50" style="WIDTH: 100%; height:120px;" name="tema" id="tema" style="WIDTH: 500px; HEIGHT: 120px"><br><br>
+		<span>Detalles:</span><input type="text" placeholder="Cuentanos mas de tu tema" size="50" style="WIDTH: 100%; height:120px;" name="tema" id="tema" style="WIDTH: 500px; HEIGHT: 120px"><br><br>
 		<input type="submit" name="submit" value="Enviar!" onclick=alert("Tema-Creado")  >
 		
 	</form>
