d20e7c14624ff154f8fc0e55897fd07f48adc6ca
Maximiliano <mcartes2017@alu.uct.cl>
2018-11-20 13:04:42 -0300

    maquetado popup reflexion

--- resources/views/createreflex.blade.php
+++ resources/views/createreflex.blade.php
@@ -3,9 +3,20 @@
 
 @section('crear-reflexion')
 
-<div class="container">
-    <h1>Crear Reflexiones</h1>
-    <form class="form-group" action="/reflexion" method="POST">
+<div class="w3-container">
+  <h2>ABRIR POP-UP</h2>
+
+
+  <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-black">Open Modal</button>
+
+  <div id="id01" class="w3-modal">
+    <div class="w3-modal-content w3-card-4">
+      <header class="w3-container w3-teal"> 
+        <span onclick="document.getElementById('id01').style.display='none'" 
+        class="w3-button w3-display-topright">&times;</span>
+        <h2>Crear Reflexiones</h2>
+      </header>
+      <form class="form-group" id="formid" action="/reflexion" method="POST">
          {!!csrf_field() !!} 
          <span> Ingresa tu Nombre</span>
         <input type="text" name="nom" id="nom">  <br> 
@@ -15,6 +26,9 @@
         <br> 
         <input type="submit" name="submit" value="Enviar!" onclick=alert("Reflexion-Enviada")  >
     </form>
+    </div>
+  </div>
 </div>
+
 
 @stop  
