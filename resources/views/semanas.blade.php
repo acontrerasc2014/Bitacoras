edd88324856a5722d466f4da3226195ff39d66d1
Diego  Zapata Tirapegui <dzapata2017@alu.uct.cl>
2018-11-24 23:45:04 -0300

    actualización mas llamativa para semanas :D

--- resources/views/semanas.blade.php
+++ resources/views/semanas.blade.php
@@ -3,10 +3,11 @@
 @section('ver-semana')
     <div class="container">
 
-    <h1 align='center'> Semanas </h1>
+    <h1 style="text-align:center;"> Semanas </h1>
     <h3>Abajo podras ver los temas de las siguientes 6 semanas:</h3>
 
-        <h8>Estos son los temas de la Semana 1:</h8>
+        <div class="card-header"><h2 style="font-weight: bold;">Semana 1:</h2></div>
+        <div class="card-body">
         <p name="Tema1">1.- Derivadas e Integrales</p>
         <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
         <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
@@ -25,35 +26,117 @@
         <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
         <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
         <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
+        </div>
 
-        <br>
-        <h8>Estos son los temas de la Semana 2:</h8>
+        <div class="card-header"><h2 style="font-weight: bold;">Semana 2:</h2></div>
+        <div class="card-body">
         <p name="Tema4">1.- Programacion en Objetos</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
         <p name="Tema5">2.- Programacion Web</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
         <p name="Tema6">3.- Inteligencia Artificial</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
+        </div>
 
-
-
-        <h8>Estos son los temas de la Semana 3:</h8>
-        <p name="Tema7">1.- Logica</p>
+        <div class="card-header"><h2 style="font-weight: bold;">Semana 3:</h2></div>
+        <div class="card-body">
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
         <p name="Tema8">2.- Circuitos integrados</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
         <p name="Tema9">3.- Circuitos Logicos</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
+        </div>
 
-        <h8>Estos son los temas de la Semana 4:</h8>
+        <div class="card-header"><h2 style="font-weight: bold;">Semana 4:</h2></div>
+        <div class="card-body">
         <p name="Tema10">1.- Diseño Web</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
         <p name="Tema11">2.- Jquery y Ajax</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
         <p name="Tema12">3.- JavaScrit</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
+        </div>
 
-        <h8>Estos son los temas de la Semana 5:</h8>
+        <div class="card-header"><h2 style="font-weight: bold;">Semana 5:</h2></div>
+        <div class="card-body">
         <p name="Tema13">1.- Python</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
         <p name="Tema14">2.- C##</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
         <p name="Tema15">3.- VisualBasics</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
+        </div>
 
-        <h8>Estos son los temas de la Semana 6:</h8>
+        <div class="card-header"><h2 style="font-weight: bold;">Semana 6:</h2></div>
+        <div class="card-body">
         <p name="Tema16">1.- Bases de Datos</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
         <p name="Tema17">2.- Eliminar y Actualizar Campos</p>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
         <p name="Tema18">3.- Editar Columnas y Filas</p>
-
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/verpreguntasrespuestas' " style="margin-bottom:20px">Ver Preguntas y Respuestas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/vercomentarios'" style="margin-bottom:20px">Ver Comentarios</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href=''" style="margin-bottom:20px">Ver Reflexiones</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/cita'" style="margin-bottom:20px">Citas</button>
+        <button type="button" class="btn btn-dark" onclick="window.location.href='/reflexion'" style="margin-bottom:20px">(Dentro de Ver Reflexiones)</button>
+        </div>
+        
         <button type="button" class="btn btn-dark" onclick="window.location.href='/tema'" style="margin:0 auto;width:200px;display: flex;align-items: center;justify-content: center; margin-top:30px;">Crear Tema</button>
     </div>
 @endsection              
